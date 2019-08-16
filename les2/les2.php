3fba96d4a9a4ae86453e24212b6df6760aac4b30
DNemercev <mikemsc@DESKTOP-7H1FKK0.localdomain>
2019-08-16 11:56:45 +0300

    mod3

--- les2/les2.php
+++ les2/les2.php
@@ -9,7 +9,11 @@ echo "Меня зовут: ", NAME, "</br>";
 echo "Мне $age года<br/>";
 unset($age);
 if (isset($age))
+{
     echo "Переменная age существует<br/>";
+}
 else
+{
     echo "Переменная age была успешно удалена";
+}
 ?>
