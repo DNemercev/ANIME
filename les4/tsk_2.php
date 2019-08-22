5c29a4898b6f2316645cb53c7486a58c290be3dd
DNemercev <mikemsc@DESKTOP-7H1FKK0.localdomain>
2019-08-23 01:02:53 +0300

    tsk 2

---
+++ les4/tsk_2.php
@@ -0,0 +1,25 @@
+<?php
+$array = array(
+    'a1' => array('id'=>'1', 'age'=>'16', 'gender'=>'m', 'login'=>'Вася'),
+    'a2' => array('id'=>'2', 'age'=>'18', 'gender'=>'m', 'login'=>'Петя'),
+    'a3' => array('id'=>'3', 'age'=>'20', 'gender'=>'g', 'login'=>'Катя'),
+    'a4' => array('id'=>'4', 'age'=>'20', 'gender'=>'m', 'login'=>'Стас'),
+    'a5' => array('id'=>'5', 'age'=>'12', 'gender'=>'g', 'login'=>'Маша'),
+    'a6' => array('id'=>'6', 'age'=>'44', 'gender'=>'g', 'login'=>'Галя'),
+    'a7' => array('id'=>'7', 'age'=>'45', 'gender'=>'m', 'login'=>'Макс'),
+    'a8' => array('id'=>'8', 'age'=>'20', 'gender'=>'m', 'login'=>'Илья'),
+    'a9' => array('id'=>'9', 'age'=>'20', 'gender'=>'g', 'login'=>'Даша'), );
+function str_cmp($a, $b)
+{
+    return strcmp($a["age"], $b["age"]);
+}
+function rev_str_cmp($a, $b)
+{
+    return ($a["login"] < $b["login"]) ? -1 : 1;
+}
+usort($array, "str_cmp");
+print_r($array);
+echo "<br/><br/>";
+usort($array,"rev_str_cmp");
+print_r($array);
+?>
