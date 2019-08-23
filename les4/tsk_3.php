78acea51355ffddae25ddaf8e520480babfe5b61
DNemercev <mikemsc@DESKTOP-7H1FKK0.localdomain>
2019-08-23 13:38:48 +0300

    mod_1

--- les4/tsk_3.php
+++ les4/tsk_3.php
@@ -1,16 +1,9 @@
 <?php
-$a = 150;
-$key = [150];
-while($a > 90)
-{
-    $a -= 3;
-    array_push($key, $a);
-}
-//var_dump($key);
+$key = range(150, 90, 3);
 $arr = array_combine($key, array_reverse($key));
-var_dump($arr);
-echo "<br/><br/>";
 $arr_2 = $arr;
 ksort($arr_2);
+var_dump($arr);
+echo "<br/><br/>";
 var_dump($arr_2);
 ?>
