efbae63c6355c79587fb059001eb0234eaee0607
DNemercev <mikemsc@DESKTOP-7H1FKK0.localdomain>
2019-08-29 00:13:22 +0300

    mod 2.0

--- les5/les5_tsk8/Sastantua/Sastantua.php
+++ les5/les5_tsk8/Sastantua/Sastantua.php
@@ -15,6 +15,95 @@ function spase($num)
 	}
 	return $k;
 }
+function last_stage ($j, $k, $all_spase)
+{
+	$m = 0;
+	$l = 0;
+	$l_k = 0;
+	if ( $j == 3)
+	{
+		while($l < $j)
+		{
+			while ($m < $all_spase)
+			{
+				echo " .";
+				$m++;
+			}
+			$m = 0;
+			$all_spase--;
+			echo "/";
+			while($l_k < $k)
+			{
+				if ($l == 2 && $l_k == 2)
+					echo "|";
+				else
+					echo "*";
+				$l_k++;
+			}
+			echo "\\<br/>";
+			$l_k = 0;
+			$k = $k + 2;
+			$l++;
+		}
+	}  
+	elseif ($j == 4 || $j == 5)
+	{
+		while ($l < $j)
+		{
+			while ($m < $all_spase)
+			{
+				echo "q";
+				$m++;
+			}
+			$m = 0;
+			$all_spase--;
+			echo "/";
+			while ($l_k < $k)
+			{
+				if (($l_k > ($k - 4)/2) && ($l_k < $k/2 + 1) && ($l > 0))
+					echo "| ";
+				else
+					echo "*";
+				$l_k++;
+			}
+			echo "\\<br/>";
+			$l_k = 0;
+			$k = $k + 2;
+			$l++;
+		}
+	}
+	else
+	{
+		while ($l < $j)
+		{
+			while ($m < $all_spase)
+			{
+				echo "q";
+				$m++;
+			}
+			$m = 0;
+			$all_spase--;
+			echo "/";
+			while ($l_k < $k)
+			{
+				if (($j - $l <= 5) && ($l_k > ($k - 6)/2) && ($l_k < $k/2 + 2))
+				{
+					if (($j - $l == 3) && ($l_k > ($k - 1)/2) && ($l_k < ($k + 2)/2))
+						echo "$";
+					else
+						echo "| ";
+				}
+				else
+					echo "*";
+				$l_k++;
+			}
+			echo "\\<br/>";
+			$l_k = 0;
+			$k = $k + 2;
+			$l++;
+		}
+	}
+}
 function sastantua($num_of_tiers)
 {
 	$all_spase = spase($num_of_tiers);
@@ -23,14 +112,14 @@ function sastantua($num_of_tiers)
 	$k = 1;
 	$m = 0;
 	$l_k = 0;
-	while ($i < $num_of_tiers)
+	while ($i < ($num_of_tiers - 1))
 	{
 		$l = 0;
 		while ($l < $j)
 		{
-			while ($m <= $all_spase)
+			while ($m < $all_spase)
 			{
-				echo " ";
+				echo "q";
 				$m++;
 			}
 			echo "/";
@@ -39,7 +128,7 @@ function sastantua($num_of_tiers)
 				echo "*";
 				$l_k++;
 			}
-			echo "\\\n";
+			echo "\\<br/>";
 			$l_k = 0;
 			$m = 0;
 			$all_spase--;
@@ -51,6 +140,9 @@ function sastantua($num_of_tiers)
 		$j++;
 		$i++;
 	}
+	last_stage ($j, $k, $all_spase);
 	return 0;
 }
+sastantua(7);
 ?>
+
